578381cfa2822b85530d98999fceaa539f766aa4
Student <example@example.com>
2019-10-07 15:49:48 -0700

    add FriendLetter project

---
+++ FriendLetter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FriendLetter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
